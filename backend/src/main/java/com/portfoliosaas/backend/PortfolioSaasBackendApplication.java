03ea549991369a7653282a966dcb942c892cb1a7
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-18 15:21:19 +0530

    init

---
+++ backend/src/main/java/com/portfoliosaas/backend/PortfolioSaasBackendApplication.java
@@ -0,0 +1,15 @@
+package com.portfoliosaas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class PortfolioSaasBackendApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // loads .env automatically
+		SpringApplication.run(PortfolioSaasBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/portfoliosaas/backend/PortfolioSaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliosaas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
