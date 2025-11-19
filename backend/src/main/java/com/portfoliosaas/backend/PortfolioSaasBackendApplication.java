c0b9dc4dc0d62d6149b41d03f1faefbf2af4b813
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-19 18:00:56 +0530

    n

--- backend/src/main/java/com/portfoliosaas/backend/PortfolioSaasBackendApplication.java
+++ backend/src/main/java/com/portfoliosaas/backend/PortfolioSaasBackendApplication.java
@@ -8,6 +8,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class PortfolioSaasBackendApplication {
 
 	public static void main(String[] args) {
+
 		Dotenv dotenv = Dotenv.load(); // loads .env automatically
 		SpringApplication.run(PortfolioSaasBackendApplication.class, args);
 	}
